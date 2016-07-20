7c157f89f8b4daaf24ac315c60629605d397b931
Richard Sheng <rcsheng@gmail.com>
2016-07-19 22:47:46 -0400

    updated with swig and express

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 var express = require('express');
+var swig = require('swig');
 var app = express();
+app.engine('html', swig.renderFile);
+app.set('view engine','html');
+app.set('views', __dirname + '/views');
+app.set('view cache',false);
+swig.setDefaults({cache: false});
 
-var mid = function (req, res, next) {
-  req.returnString = req.method + " \\" + req.baseUrl; 
-  next();
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
 };
+// swig.renderFile(__dirname + '/views/index.html', locals, function (err, output) {
+//     console.log(output);
+// });
+// var mid = function (req, res, next) {
+//   req.returnString = req.method + " \\" + req.baseUrl; 
+//   next();
+// };
 
-app.use(mid);
+// app.use(mid);
 
 app.get('/', function (req, res) {
-  var responseText = 'Hello World!<br>';
-  responseText += '<small>Requested at: ' + req.returnString + '</small>';
-  res.send(responseText);
+  // var responseText = 'Hello World!<br>';
+  // responseText += '<small>Requested at: ' + req.returnString + '</small>';
+  // res.send(responseText);
+  res.render('index', locals);
 });
 
 app.listen(3000);
